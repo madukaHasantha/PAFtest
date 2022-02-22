48134a1fa342dfe7b2f2f7bf6d3229f4f1e983ad
madukaHasantha <mhasantha2@gmail.com>
2022-02-22 18:49:33 +0530

    first commit

---
+++ lab02Faf/src/lab02Faf/main.java
@@ -0,0 +1,14 @@
+package lab02Faf;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("hello sliit");
+
+	}
+	
+	
+}
